ea070e9cffe7acb23c65aaabb38577a593037541
KavithaE09 <kavithae704@gmail.com>
2025-11-20 21:27:17 +0530

    final commit

--- project/src/api/api.js
+++ project/src/api/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://college-dashboard-training.onrender.com/api";
+
 
 // Helper function for API calls
 const apiCall = async (endpoint, options = {}) => {
